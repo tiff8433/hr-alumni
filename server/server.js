7e50e689f0ed9c1fb65a766c47eef3769eeba686
Ben Grinberg <bengrin25@gmail.com>
2016-01-07 15:05:52 -0500

    Procfile

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ var port = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + '/../client'));
+
 app.listen(3000, function() {
   console.log('Server started on port: ' + port);
 });
