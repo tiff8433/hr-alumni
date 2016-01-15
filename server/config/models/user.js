0cb85fc09b2829e3d8df1d88eb1674982651943a
Robert Lin <Rlin415@gmail.com>
2016-01-15 08:01:23 -0800

    update user model

--- server/config/models/user.js
+++ server/config/models/user.js
@@ -1,7 +1,16 @@
-var db = require('../config.js');
+var db = require('../config.js'),
+    Post = require('./post.js'),
+    Reply = require('./reply.js');
+
 
 var User = db.Model.extend({
-  tableName: 'users'
+  tableName: 'users',
+  post: function() {
+    return this.hasMany(Post);
+  },
+  reply: function() {
+    return this.hasMany(Reply);
+  }
 });
 
 module.exports = User;
