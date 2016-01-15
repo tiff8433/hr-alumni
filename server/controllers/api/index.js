b4fcba8bb73ad1c43d171125767d7ef3b9cee2bf
Robert Lin <Rlin415@gmail.com>
2016-01-15 15:41:12 -0800

    complete post handler

--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -1,13 +1,14 @@
 var express = require('express'),
-    util = require('../../config/utils.js');
+    util = require('../../config/utils.js'),
+    post = require('./post.controller.js');
 
 var apiRouter = express.Router();
 
-apiRouter.get('/posts', util.checkUser);
-apiRouter.get('/post/read/:id', util.checkUser);
+apiRouter.get('/posts', util.checkUser, post.getAllPosts);
+apiRouter.get('/post/read/:id', util.checkUser, post.getPost);
 apiRouter.get('/post/replies/:id', util.checkUser);
 apiRouter.get('/category/:category', util.checkUser);
-apiRouter.post('/post/create', util.checkUser);
+apiRouter.post('/post/create', util.checkUser, post.createPost);
 apiRouter.post('/post/replies/:id', util.checkUser);
 apiRouter.put('/post/read/:id/upvote', util.checkUser);
 apiRouter.put('/post/replies/:id/thumb', util.checkUser);
