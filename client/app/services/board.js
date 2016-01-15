cc3c2ee85781d9d0e4b686f67a184e3a32d3e35b
tiff8433 <tiffany.l.huang@gmail.com>
2016-01-14 22:21:48 -0800

    Implements board, post, and reply services and controller

--- client/app/services/board.js
+++ client/app/services/board.js
@@ -6,6 +6,10 @@ angular.module('board.services', [])
     events: 'calendar'
   };
 
+  var categoryIcons = {
+    events: 'calendar'
+  };
+
   var posts = [
     {
       id: 'abcdefg12345',
@@ -13,8 +17,13 @@ angular.module('board.services', [])
       user: 'Robert Lin',
       posted: '2 days ago',
       category: 'events',
+<<<<<<< e0d05cb300f66556568f634e92a3d634ca26e70d
       replyCount: 2,
       categoryIcon: categoryIcons['events'],
+=======
+      categoryIcon: categoryIcons[this.category],
+      content: "Gochujang echo park post-ironic hoodie, iPhone tousled slow-carb pickled kombucha aesthetic ennui bushwick pour-over. Cliche shoreditch chartreuse, vice occupy butcher crucifix hashtag pop-up pour-over. Keytar you probably haven't heard of them artisan taxidermy, photo booth kickstarter tattooed. Williamsburg occupy biodiesel 3 wolf moon before they sold out organic salvia, meditation flexitarian whatever ethical flannel. Celiac mustache art party post-ironic PBR&B thundercats. Pug polaroid ugh pop-up vegan. Letterpress humblebrag you probably haven't heard of them, mlkshk tousled viral raw denim church-key heirloom iPhone lumbersexual hoodie blog.",
+>>>>>>> Implements board, post, and reply services and controller
       hearts: '37'
     },
     {
