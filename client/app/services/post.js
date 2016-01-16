4319ae5290c0b5b34a47e5069f0c704cf894549f
Brian Tedder <imbrianted@gmail.com>
2016-01-16 12:20:36 -0700

    uncommented fake client data

--- client/app/services/post.js
+++ client/app/services/post.js
@@ -1,17 +1,6 @@
 angular.module('post.services', [])
 .factory('Post', ['$http', '$state', 'Reply', function($http, $state, Reply) {
 
-  // var posts = [
-  //   {
-  //     id: 'abcdefg12345',
-  //     content: "Gochujang echo park post-ironic hoodie, iPhone tousled slow-carb pickled kombucha aesthetic ennui bushwick pour-over. Cliche shoreditch chartreuse, vice occupy butcher crucifix hashtag pop-up pour-over. Keytar you probably haven't heard of them artisan taxidermy, photo booth kickstarter tattooed. Williamsburg occupy biodiesel 3 wolf moon before they sold out organic salvia, meditation flexitarian whatever ethical flannel. Celiac mustache art party post-ironic PBR&B thundercats. Pug polaroid ugh pop-up vegan. Letterpress humblebrag you probably haven't heard of them, mlkshk tousled viral raw denim church-key heirloom iPhone lumbersexual hoodie blog."
-  //   },
-  //   {
-  //     id: 'abcdefg12346',
-  //     content: "Gochujang echo park post-ironic hoodie, iPhone tousled slow-carb pickled kombucha aesthetic ennui bushwick pour-over. Cliche shoreditch chartreuse, vice occupy butcher crucifix hashtag pop-up pour-over. Keytar you probably haven't heard of them artisan taxidermy, photo booth kickstarter tattooed. Williamsburg occupy biodiesel 3 wolf moon before they sold out organic salvia, meditation flexitarian whatever ethical flannel. Celiac mustache art party post-ironic PBR&B thundercats. Pug polaroid ugh pop-up vegan. Letterpress humblebrag you probably haven't heard of them, mlkshk tousled viral raw denim church-key heirloom iPhone lumbersexual hoodie blog."
-  //   }
-  // ];
-
   var getReplies = function(postId) {
     return $http({
       method: 'GET',
