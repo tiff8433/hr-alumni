932f858b52f24c92e487fa8644c2cdeb3b9a9378
Ryan Jagger <ryanjagger@gmail.com>
2016-01-15 21:31:02 -0600

    Added category controller to router

--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -1,5 +1,6 @@
 var express = require('express'),
     util = require('../../config/utils.js'),
+    category = require('./category.controller'),
     reply = require('./reply.controller');
 
 var apiRouter = express.Router();
@@ -8,7 +9,7 @@ apiRouter.get('/posts', util.checkUser);
 apiRouter.get('/post/read/:id', util.checkUser);
 apiRouter.put('/post/read/:id/upvote', util.checkUser);
 apiRouter.get('/post/replies/:id', reply.getReplies);
-apiRouter.get('/category/:category', util.checkUser);
+apiRouter.get('/category/:category', category.getPostsByCategory);
 apiRouter.post('/post/create', util.checkUser);
 apiRouter.post('/post/replies/:id', reply.postReply);
 apiRouter.put('/post/replies/:id/thumb', reply.thumbUpReply);
