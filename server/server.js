c85d3ea24aeb4a7c09b0913e3991e1facb0b5f53
Brian Tedder <imbrianted@gmail.com>
2016-01-16 11:26:22 -0700

    updated gitignore to externalize owner's credentials

--- server/server.js
+++ server/server.js
@@ -14,14 +14,14 @@ var app = express();
 
 
   // Express 4 allows us to use multiple routers with their own configurations
-  var questionsRouter = express.Router();
+  //var questionsRouter = express.Router();
 
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
   app.use(express.static(__dirname + '/../client'));
 
 
-  app.use('/api/profiles', questionsRouter); // use questions router for all questions request
+  //app.use('/api/profiles', questionsRouter); // use questions router for all questions request
 
 
   // inject our routers into their respective route files
