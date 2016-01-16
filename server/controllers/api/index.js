5e6aefb466958c6dd07cb66699f2e423a9b7f202
Brian Tedder <imbrianted@gmail.com>
2016-01-15 19:43:14 -0700

    hooked up reply controllers

--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -10,7 +10,7 @@ apiRouter.put('/post/read/:id/upvote', util.checkUser);
 apiRouter.get('/post/replies/:id', reply.getReplies);
 apiRouter.get('/category/:category', util.checkUser);
 apiRouter.post('/post/create', util.checkUser);
-apiRouter.post('/post/replies/:id', util.checkUser);
-apiRouter.put('/post/replies/:id/thumb', util.checkUser);
+apiRouter.post('/post/replies/:id', reply.postReply);
+apiRouter.put('/post/replies/:id/thumb', reply.thumbUpReply);
 
 module.exports = apiRouter;
