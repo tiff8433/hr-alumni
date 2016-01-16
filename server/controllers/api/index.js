b2b2edd44ee011ea4ddaf4856de5eb7628967778
Robert Lin <Rlin415@gmail.com>
2016-01-15 19:22:22 -0800

    fix changes

--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -11,15 +11,12 @@ apiRouter.post('/post/create', post.createPost);
 apiRouter.put('/post/read/upvote/:id', post.upVote);
 
 apiRouter.get('/post/replies/:id', reply.getReplies);
-apiRouter.put('/post/replies/:id/thumb');
 
 apiRouter.get('/category/:category');
 
 
 apiRouter.post('/post/replies/:id', reply.postReply);
 apiRouter.put('/post/replies/:id/thumb', reply.thumbUpReply);
-apiRouter.post('/post/create', util.checkUser, post.createPost);
-apiRouter.put('/post/read/:id/upvote', util.checkUser);
 
 
 module.exports = apiRouter;
