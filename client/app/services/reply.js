758a07a2a157af9314d9908d90b0a2f5f8312273
Brian Tedder <imbrianted@gmail.com>
2016-01-16 12:18:20 -0700

    connected factories

--- client/app/services/reply.js
+++ client/app/services/reply.js
@@ -2,54 +2,54 @@ angular.module('reply.services', [])
 
 .factory('Reply', ['$http', '$state', function($http, $state) {
   
-  var replies = [
-    {
-      id: 'abcdefg12345',
-      replies: [
-        {
-          id: '987654321',
-          user: 'Tiffany Huang',
-          image: '/u/11264591?v=3&s=460',
-          //https://avatars0.githubusercontent.com
-          content: "Wayfarers food truck small batch cronut pour-over. 3 wolf moon pour-over cronut brooklyn, migas viral aesthetic try-hard readymade intelligentsia kombucha cred shabby chic.",
-          posted: '8 min ago',
-          thumbs: 8
-        },
-        {
-          id: '987654322',
-          user: 'Tiffany Huang',
-          image: '/u/11264591?v=3&s=460',
-          //https://avatars0.githubusercontent.com
-          content: "Wayfarers food truck small batch cronut pour-over. 3 wolf moon pour-over cronut brooklyn, migas viral aesthetic try-hard readymade intelligentsia kombucha cred shabby chic.",
-          posted: '8 min ago',
-          thumbs: 8
-        }
-      ]
-    },
-    {
-      id: 'abcdefg12346',
-      replies: [
-        {
-          id: '987654327',
-          user: 'Tiffany Huang',
-          image: '/u/11264591?v=3&s=460',
-          //https://avatars0.githubusercontent.com
-          content: "Wayfarers food truck small batch cronut pour-over. 3 wolf moon pour-over cronut brooklyn, migas viral aesthetic try-hard readymade intelligentsia kombucha cred shabby chic.",
-          posted: '8 min ago',
-          thumbs: 8
-        },
-        {
-          id: '987654328',
-          user: 'Tiffany Huang',
-          image: '/u/11264591?v=3&s=460',
-          //https://avatars0.githubusercontent.com
-          content: "Wayfarers food truck small batch cronut pour-over. 3 wolf moon pour-over cronut brooklyn, migas viral aesthetic try-hard readymade intelligentsia kombucha cred shabby chic.",
-          posted: '8 min ago',
-          thumbs: 8
-        }
-      ]
-    }
-  ];
+  // var replies = [
+  //   {
+  //     id: 'abcdefg12345',
+  //     replies: [
+  //       {
+  //         id: '987654321',
+  //         user: 'Tiffany Huang',
+  //         image: '/u/11264591?v=3&s=460',
+  //         //https://avatars0.githubusercontent.com
+  //         content: "Wayfarers food truck small batch cronut pour-over. 3 wolf moon pour-over cronut brooklyn, migas viral aesthetic try-hard readymade intelligentsia kombucha cred shabby chic.",
+  //         posted: '8 min ago',
+  //         thumbs: 8
+  //       },
+  //       {
+  //         id: '987654322',
+  //         user: 'Tiffany Huang',
+  //         image: '/u/11264591?v=3&s=460',
+  //         //https://avatars0.githubusercontent.com
+  //         content: "Wayfarers food truck small batch cronut pour-over. 3 wolf moon pour-over cronut brooklyn, migas viral aesthetic try-hard readymade intelligentsia kombucha cred shabby chic.",
+  //         posted: '8 min ago',
+  //         thumbs: 8
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     id: 'abcdefg12346',
+  //     replies: [
+  //       {
+  //         id: '987654327',
+  //         user: 'Tiffany Huang',
+  //         image: '/u/11264591?v=3&s=460',
+  //         //https://avatars0.githubusercontent.com
+  //         content: "Wayfarers food truck small batch cronut pour-over. 3 wolf moon pour-over cronut brooklyn, migas viral aesthetic try-hard readymade intelligentsia kombucha cred shabby chic.",
+  //         posted: '8 min ago',
+  //         thumbs: 8
+  //       },
+  //       {
+  //         id: '987654328',
+  //         user: 'Tiffany Huang',
+  //         image: '/u/11264591?v=3&s=460',
+  //         //https://avatars0.githubusercontent.com
+  //         content: "Wayfarers food truck small batch cronut pour-over. 3 wolf moon pour-over cronut brooklyn, migas viral aesthetic try-hard readymade intelligentsia kombucha cred shabby chic.",
+  //         posted: '8 min ago',
+  //         thumbs: 8
+  //       }
+  //     ]
+  //   }
+  // ];
 
   var upvoteReply = function(replyId) {
     return $http({
@@ -71,7 +71,7 @@ angular.module('reply.services', [])
   };
 
   return {
-    replies: replies,
+    upvoteReply: upvoteReply,
     postReply: postReply
   };
 }]);
