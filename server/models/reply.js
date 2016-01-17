4efd5d580284a7996f312bea31d5d790a81ec572
Brian Tedder <imbrianted@gmail.com>
2016-01-16 18:09:41 -0700

    model changes

--- server/models/reply.js
+++ server/models/reply.js
@@ -1,15 +1,15 @@
 var db = require('../config/config.js'),
-    User = require('./user.js'),
-    Post = require('./post.js');
+    User = require('./index').User,
+    Post = require('./index').Post;
 
 var Reply = db.Model.extend({
   tableName: 'replies',
   hasTimestamps: true,
-  user: function() {
-    return this.belongsTo(User, 'user_id');
-  },
   post: function() {
     return this.belongsTo(Post, 'post_id');
+  },
+  user: function() {
+    return this.belongsTo(User, 'user_id');
   }
 });
 
