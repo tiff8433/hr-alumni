e0c2de665e58fd5657da8d7bc1f0edcb8ff333e9
Ryan Jagger <ryanjagger@gmail.com>
2016-01-18 17:54:34 -0600

    https to http

--- server/server.js
+++ server/server.js
@@ -45,13 +45,13 @@ passport.deserializeUser(function(obj, done) {
 // if (process.env.NODE_ENV === undefined) {
 //   var callbackURL = 'http://localhost:3000/auth/github/callback';
 // } else {
-//   var callbackURL = 'https://hr-alumni-app.herokuapp.com/auth/github/callback';
+//   var callbackURL = 'http://hr-alumni-app.herokuapp.com/auth/github/callback';
 // }
 
 passport.use(new GithubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID || secrets.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET || secrets.GITHUB_CLIENT_SECRET,
-    callbackURL: 'https://hr-alumni-app.herokuapp.com/auth/github/callback'
+    callbackURL: 'http://hr-alumni-app.herokuapp.com/auth/github/callback'
   },
   function(accessToken, refreshToken, profile, done) {
     process.nextTick(function() {
